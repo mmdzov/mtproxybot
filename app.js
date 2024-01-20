6ea4b32756f733cf7873dc465ec75939271bbfbf
mmdzov <mzov939@gmail.com>
2024-01-20 14:42:33 +0330

    Update

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ const initial = () => ({
   expiryDateUsername: "",
   expiryDateId: -1,
   waitForExpiryDateMsgIds: [],
+
+  waitForQuotaOption: false,
+  quotaOptionUsername: "",
+  quotaOptionId: -1,
+  waitForQuotaOptionMsgIds: [],
 });
 
 bot.use(
@@ -124,6 +129,44 @@ Expire at: unlimited
     ctx.session.waitForExpiryDateMsgIds = [];
   });
 
+  const backToQuotaOptionMenu = new Menu("back-to-quota-option")
+  .back("<< Back", (ctx) => {
+    ctx.editMessageText("Select a user:");
+  })
+  .text("Clear", async (ctx) => {
+    const result = execSync(`${scripts.run} 7`, {
+      input: `${ctx.session.quotaOptionId}\n\n`,
+    }).toString();
+
+    const msgId = ctx.callbackQuery.message.message_id;
+
+    try {
+      await ctx.deleteMessages([msgId, ...ctx.session.waitForQuotaOptionMsgIds]);
+    } catch (e) {}
+
+    await ctx.reply(
+      `
+quota options successfully changed: 
+<pre>
+User: ${ctx.session.quotaOptionUsername} 
+Quota options: unlimited
+
+</pre>`,
+      {
+        parse_mode: "HTML",
+      },
+    );
+
+    ctx.reply("Select a user:", {
+      reply_markup: quotaOptionMenu,
+    });
+
+    ctx.session.waitForQuotaOption = false;
+    ctx.session.quotaOptionId = -1;
+    ctx.session.quotaOptionUsername = "";
+    ctx.session.waitForQuotaOptionMsgIds = [];
+  });
+
 const backToMainMenu = new Menu("back-to-main").back("<< Back", (ctx) => {
   ctx.editMessageText("select an option:");
 });
@@ -131,6 +174,7 @@ const backToMainMenu = new Menu("back-to-main").back("<< Back", (ctx) => {
 bot.use(backToMainMenu);
 bot.use(backToLimitConnectionMenu);
 bot.use(backToExpiryDateMenu);
+bot.use(backToQuotaOptionMenu);
 
 const revokeSecretMenu = new Menu("revoke-secret")
   .dynamic(async (dctx) => {
@@ -301,6 +345,63 @@ Enter the expiry date in format of day/month/year(Example 11/09/2019)
     ctx.editMessageText("select an option:");
   });
 
+  const quotaOptionMenu = new Menu("quota-option")
+  .dynamic(async (dctx) => {
+    let proxies = "";
+
+    try {
+      proxies = execSync(`${scripts.run} 1`).toString();
+    } catch (e) {}
+
+    if (!proxies || !proxies?.trim()) {
+      await dctx.answerCallbackQuery({
+        text: "There is no proxy yet",
+      });
+      return;
+    }
+
+    const users = proxies
+      .split("\n")
+      .filter((item) => item)
+      .slice(1)
+      .map((item) => item.split(" ")[0].split(":").join(""));
+
+    const range = new MenuRange();
+
+    for (let i in users) {
+      const user = users[i];
+
+      range
+        .text(user, async (ctx) => {
+          const proxyIndex = +i + 1;
+
+          const res = await ctx.editMessageText(
+            `
+Enter the limit of the user in bytes. You can use kb, mb and gb suffixes
+          `,
+            {
+              reply_markup: backToQuotaOptionMenu,
+            },
+          );
+
+          ctx.session.waitForQuotaOption = true;
+          ctx.session.quotaOptionId = proxyIndex;
+          ctx.session.quotaOptionUsername = user;
+          ctx.session.waitForQuotaOptionMsgIds = [
+            res.message_id,
+            ctx.callbackQuery.message.message_id,
+          ];
+        })
+        .row();
+    }
+
+    return range;
+  })
+  .row()
+  .back("<< Back", (ctx) => {
+    ctx.editMessageText("select an option:");
+  });
+
 const mainMenu = new Menu("main-menu")
   .text("View all links", async (ctx) => {
     let proxies = "";
@@ -445,6 +546,23 @@ Warning! Do not use special characters like " , ' , $ or... for username
     ctx.editMessageText("Select a user:", {
       reply_markup: expiryDateMenu,
     });
+  }).row().text("Quota options",async (ctx) => {
+    let proxies = "";
+
+    try {
+      proxies = execSync(`${scripts.run} 1`).toString();
+    } catch (e) {}
+
+    if (!proxies || !proxies?.trim()) {
+      await ctx.answerCallbackQuery({
+        text: "There is no proxy yet",
+      });
+      return;
+    }
+
+    ctx.editMessageText("Select a user:", {
+      reply_markup: quotaOptionMenu,
+    });
   });
 
 const addSecretMenu = new Menu("add-secret")
@@ -506,14 +624,17 @@ bot.use(mainMenu);
 bot.use(addSecretMenu);
 bot.use(limitConnectionMenu);
 bot.use(expiryDateMenu);
+bot.use(quotaOptionMenu);
 
 mainMenu.register(backToMainMenu);
 mainMenu.register(addSecretMenu);
 mainMenu.register(revokeSecretMenu);
 mainMenu.register(limitConnectionMenu);
 mainMenu.register(expiryDateMenu);
+mainMenu.register(quotaOptionMenu);
 limitConnectionMenu.register(backToLimitConnectionMenu);
 expiryDateMenu.register(backToExpiryDateMenu);
+quotaOptionMenu.register(backToQuotaOptionMenu);
 
 bot.command("reset", (ctx) => {
   ctx.session = {
@@ -536,6 +657,11 @@ bot.command("reset", (ctx) => {
     expiryDateUsername: "",
     expiryDateId: -1,
     waitForExpiryDateMsgIds: [],
+
+    waitForQuotaOption: false,
+    quotaOptionUsername: "",
+    quotaOptionId: -1,
+    waitForQuotaOptionMsgIds: [],
   };
 
   ctx.reply("select an option:", {
@@ -650,6 +776,57 @@ Expire at: ${msg}
     ctx.session.expiryDateId = -1;
     ctx.session.expiryDateUsername = "";
     ctx.session.waitForExpiryDateMsgIds = [];
+  });
+
+  bot
+  .filter((ctx) => ctx.session.waitForQuotaOption)
+  .on("message", async (ctx) => {
+    const msg = ctx.message?.text;
+
+    if (!msg) {
+      const res = await ctx.reply("Error: Wrong quota options format", {
+        reply_markup: backToQuotaOptionMenu,
+      });
+
+      ctx.session.waitForQuotaOptionMsgIds.push(res.message_id);
+
+      return;
+    }
+
+    const result = execSync(`${scripts.run} 8`, {
+      input: `${ctx.session.quotaOptionId}\n${msg.trim()}\n`,
+    }).toString();
+
+    const msgId = ctx.message.message_id;
+
+    try {
+      await ctx.deleteMessages([msgId, ...ctx.session.waitForQuotaOptionMsgIds]);
+    } catch (e) {}
+
+    // let isDone = result.includes("Done");
+
+    // if (isDone) {
+    await ctx.reply(
+      `
+quota options successfully changed: 
+<pre>
+User: ${ctx.session.quotaOptionUsername} 
+Quota options: ${msg}
+
+</pre>`,
+      {
+        parse_mode: "HTML",
+      },
+    );
+
+    ctx.reply("Select a user:", {
+      reply_markup: quotaOptionMenu,
+    });
+
+    ctx.session.waitForQuotaOption = false;
+    ctx.session.quotaOptionId = -1;
+    ctx.session.quotaOptionUsername = "";
+    ctx.session.waitForQuotaOptionMsgIds = [];
   });
 
 bot
